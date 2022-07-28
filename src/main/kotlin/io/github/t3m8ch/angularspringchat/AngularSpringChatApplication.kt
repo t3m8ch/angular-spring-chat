ced9e810408a9d7319971616c4dc993aef87dbf6
Kudyakov Artem <t3m8ch@yandex.ru>
2022-07-28 21:05:07 +0400

    feat: Init project

---
+++ src/main/kotlin/io/github/t3m8ch/angularspringchat/AngularSpringChatApplication.kt
@@ -0,0 +1,11 @@
+package io.github.t3m8ch.angularspringchat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AngularSpringChatApplication
+
+fun main(args: Array<String>) {
+    runApplication<AngularSpringChatApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/t3m8ch/angularspringchat/AngularSpringChatApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.t3m8ch.angularspringchat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AngularSpringChatApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
